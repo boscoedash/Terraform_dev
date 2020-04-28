0488179ce6ac69b27366d2be89447cf37be4f04e
Bhaskar Lokanathan <bhlokana@microsoft.com>
2020-04-28 14:55:13 -0500

    variables.tf

--- Modules/AppService/main.tf
+++ Modules/AppService/main.tf
@@ -8,15 +8,16 @@ module "naming" {
 
 locals {
 
-  logs                        = var.logs == null ? {} : var.logs
-  storage_account_mount_path  = var.storage_account_mount_path == null ? {} : var.storage_account_mount_path
-  site_config_cors            = var.site_config_cors == null ? {} : var.site_config_cors
-  identity                    = var.identity  == null ? {} : var.identity
-  app_settings                = var.app_settings  == null ? {} : var.app_settings
-  auth_settings               = var.auth_settings  == null ? {} : var.auth_settings
-  active_directory            = var.active_directory  == null ? {} : var.active_directory
-  backup                      = var.backup  == null ? {} : var.backup
-  ip_restriction              = var.ip_restriction == null ? {} : var.ip_restriction
+  logs_map                        = var.logs_map == null ? {} : var.logs_map
+  site_config_cors_map            = var.site_config_cors_map == null ? {} : var.site_config_cors_map
+  identity_map                    = var.identity_map  == null ? {} : var.identity_map
+  app_settings_map                = var.app_settings_map  == null ? {} : var.app_settings_map
+  auth_settings_map               = var.auth_settings_map  == null ? {} : var.auth_settings_map
+  active_directory_map            = var.active_directory_map  == null ? {} : var.active_directory_map
+  storage_account_map             = var.storage_account_map  == null ? {} : var.storage_account_map
+  backup_map                      = var.backup_map  == null ? {} : var.backup_map
+  schedule_map                    = var.schedule_map  == null ? {} : var.schedule_map
+  ip_restriction_map              = var.ip_restriction_map == null ? {} : var.ip_restriction_map
 }
 
 resource "azurerm_app_service" "app_service" {
@@ -24,7 +25,7 @@ resource "azurerm_app_service" "app_service" {
   location                = local.location
   resource_group_name     = var.resource_group_name
   app_service_plan_id     = var.app_service_plan_id
-  app_settings            = local.app_settings
+  app_settings_map        = local.app_settings_map
   client_affinity_enabled = var.client_affinity_enabled
   client_cert_enabled     = var.client_cert_enabled
   enabled                 = var.enabled
@@ -32,47 +33,43 @@ resource "azurerm_app_service" "app_service" {
   tags                    = local.tags
 
   auth_settings {
-    enabled                        = local.auth_settings_enabled
-    additional_login_params        = local.auth_settings_additional_login_params
-    allowed_external_redirect_urls = local.auth_settings_allowed_external_redirect_urls
-    default_provider               = local.auth_settings_default_provider
-    runtime_version                = local.auth_settings_runtime_version
-    token_refresh_extension_hours  = local.auth_settings_token_refresh_extension_hours
-    token_store_enabled            = local.auth_settings_token_store_enabled
-    unauthenticated_client_action  = local.auth_settings_unauthenticated_client_action
-    issuer                         = local.auth_settings_isuer
+    enabled                        = local.auth_settings_map_enabled
+    additional_login_params        = local.auth_settings_map_additional_login_params
+    allowed_external_redirect_urls = local.auth_settings_map_allowed_external_redirect_urls
+    default_provider               = local.auth_settings_map_default_provider
+    runtime_version                = local.auth_settings_map_runtime_version
+    token_refresh_extension_hours  = local.auth_settings_map_token_refresh_extension_hours
+    token_store_enabled            = local.auth_settings_map_token_store_enabled
+    unauthenticated_client_action  = local.auth_settings_map_unauthenticated_client_action
+    issuer                         = local.auth_settings_map_isuer
 
     active_directory {
-      client_id         = local.active_directory.value.client_id
-      client_secret     = local.active_directory.value.client_secret
-      allowed_audiences = local.active_directory.value.client_id
+      client_id         = local.active_directory_map_client_id
+      client_secret     = local.active_directory_map_client_secret
+      allowed_audiences = local.active_directory_mapclient_id
     }
   }
 
   storage_account {
-    name         = var.storage_account_identifier
-    type         = var.storage_account_type
-    account_name = var.storage_account_name
-    share_name   = var.storage_account_share_name
-    access_key   = var.storage_account_access_key
-    mount_path   = local.storage_account_mount_path 
+    name         = lookup(local.storage_account, "name")
+    type         = lookup(local.storage_account, "type")
+    account_name = lookup(local.storage_account, "account_name")
+    share_name   = lookup(local.storage_account, "share_name")
+    access_key   = lookup(local.storage_account, "access_key")
+    mount_path   = lookup(local.storage_account, "mount_path", null)
   }
 
   backup {
-    name                = local.backup_name
-    enabled             = local.backup_enabled
-    storage_account_url = local.backup_storage_account_url == "" ? {} : local.backup_storage_account_url
+    name                = local.backup_map_name
+    enabled             = local.backup_map_enabled
+    storage_account_url = local.backup_map_storage_account_url == "" ? {} : local.backup_map_storage_account_url
 
-    dynamic "schedule" {
-      for_each backup.value.schedule == "" ? {} : backup.value.schedule
-      
-      content {
-        frequency_interval       = lookup(schedule.value, "frequency_interval")
-        frequency_unit           = lookup(schedule.value, "frequency_unit")
-        keep_at_least_one_backup = lookup(schedule.value, "keep_at_least_one_backup")
-        retention_period_in_days = lookup(schedule.value, "retention_period_in_days")
-        start_time               = lookup(schedule.value, "start_time")
-      }
+    schedule {
+      frequency_interval       = lookup(local.schedule_map.value, "frequency_interval")
+      frequency_unit           = lookup(local.schedule_map.value, "frequency_unit")
+      keep_at_least_one_backup = lookup(local.schedule_map.value, "keep_at_least_one_backup")
+      retention_period_in_days = lookup(local.schedule_map.value, "retention_period_in_days")
+      start_time               = lookup(local.schedule_map.value, "start_time")
     }
   }
 
@@ -82,49 +79,25 @@ resource "azurerm_app_service" "app_service" {
     value = var.connection_string_value
   }
 
-  dynamic "logs" {
-    for_each = local.logs
-    
-    content {
-      dynamic "application_logs" {
-        for_each = logs.value.application_logs == "" ? {} : logs.value.application_logs
+  logs {
+    application_logs_map {
+      azure_blob_storage {
+        level             = lookup(local.logs_map.value.application_logs_map.value.azure_blob_storage.value, "level", "Off")
+        sas_url           = lookup(local.logs_map.value.application_logs_map.value.azure_blob_storage.value, "sas_url")
+        retention_in_days = lookup(local.logs_map.value.application_logs_map.value.azure_blob_storage.value, "retention_in_days", 30)
+      }
+    }
 
-        content {
-          dynamic "azure_blob_storage" {
-            for_each = logs.value.application_logs.value.azure_blob_storage == "" ? {} : logs.value.application_logs.value.azure_blob_storage
-
-            content {
-              level             = lookup(azure_blob_storage.value, "level", "Off")
-              sas_url           = lookup(azure_blob_storage.value, "sas_url")
-              retention_in_days = lookup(azure_blob_storage.value, "retention_in_days", 30)
-            }
-          }
-        }
+    http_logs {
+      azure_blob_storage {
+        level             = lookup(local.logs_map.value.application_logs_map.value.azure_blob_storage.value, "level", "Off")
+        sas_url           = lookup(local.logs_map.value.application_logs_map.value.azure_blob_storage.value, "sas_url")
+        retention_in_days = lookup(local.logs_map.value.application_logs_map.value.azure_blob_storage.value, "retention_in_days", 30)
       }
 
-      dynamic "http_logs" {
-        for_each = logs.value.http_logs == "" ? {} : logs.value.http_logs
-
-        content {
-          dynamic "azure_blob_storage" {
-            for_each = logs.value.http_logs.value.azure_blob_storage == "" ? {} : logs.value.http_logs.value.azure_blob_storage
-
-            content {
-              level             = lookup(azure_blob_storage.value, "level", "Off")
-              sas_url           = lookup(azure_blob_storage.value, "sas_url")
-              retention_in_days = lookup(azure_blob_storage.value, "retention_in_days", 30)
-            }
-          }
-
-          dynamic "file_system" {
-            for_each = logs.value.http_logs.value.file_system == "" ? {} : logs.value.http_logs.value.file_system
-
-            content {
-              retention_in_mb   = lookup(file_system.value, "retention_in_mb")
-              retention_in_days = lookup(file_system.value, "retention_in_days", 30)
-            }
-          }
-        }
+      file_system {
+        retention_in_mb   = lookup(local.logs_map.value.http_logs_map.value.file_system.value, "retention_in_mb")
+        retention_in_days = lookup(local.logs_map.value.http_logs_map.value.file_system.value, "retention_in_days", 30)
       }
     }
   }
@@ -152,26 +125,22 @@ resource "azurerm_app_service" "app_service" {
     websockets_enabled        = lookup(var.site_config.value, "site_config_windows_fx_verison")
 
     dynamic "ip_restriction" {
-      for_each = local.ip_restriction
+      for_each = local.ip_restriction_map
 
       content {
-        ip_address                = lookup(ip_restriction.value, "ip_address")
-        virtual_network_subnet_id = lookup(ip_restriction.value, "virtual_network_subnet_id")
+        ip_address                = lookup(ip_restriction_map.value, "ip_address")
+        virtual_network_subnet_id = lookup(ip_restriction_map.value, "virtual_network_subnet_id")
       }
     }
 
-    dynamic "cors" {
-      for_each = local.site_config_cors
-
-      content {
-        allowed_origins     = lookup(site_config_cors.value, "allowed_origins", "*")
-        support_credentials = lookup(site_config_cors.value, "support_credentials")
-      }
+    cors {
+      allowed_origins     = lookup(local.site_config_cors_map.value, "allowed_origins", "*")
+      support_credentials = lookup(local.site_config_cors_map.value, "support_credentials")
     }
   }
 
   identity {
-    type         = lookup(local.identity.value, "type")
-    identity_ids = lookup(local.identity.value, "identity_ids", [])
+    type         = lookup(local.identity_map.value, "type")
+    identity_ids = lookup(local.identity_map.value, "identity_ids", [])
   }
 }
