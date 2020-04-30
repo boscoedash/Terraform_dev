a7ead403782ad8e59cf86d22f3ff16db4cb493d2
Bhaskar Lokanathan <bhlokana@microsoft.com>
2020-04-30 13:54:31 -0500

    Internal ASE

---
+++ Modules/InternalAppServiceEnvironmentARM/main.tf
@@ -0,0 +1,26 @@
+module "naming" {
+  source      = "../naming/standard-name"
+  counter     = var.counter
+  description = var.description
+  location    = local.location
+  type        = "ase"
+}
+
+module "ARMTemplateDeployment" {
+  name                = lower(module.naming.name)
+  resource_group_name = var.resource_group_name
+  deployment_mode     = var.deployment_mode == null ? "Incremental" : var.deployment_mode
+  template_body       = file(var.template_file)
+  parameters_body     = file(var.parameters_file)
+
+  parameters = {
+    "aseName"                             = lower(module.naming.name)
+    "aseLocation"                         = local.location
+    "existingVirtualNetworkName"          = var.existingVirtualNetworkName
+    "existingVirtualNetworkResourceGroup" = var.existingVirtualNetworkResourceGroup
+    "subnetName"                          = var.subnetName
+    "internalLoadBalancingMode"           = var.internalLoadBalancingMode == null ? 3 : var.internalLoadBalancingMode
+    "dnsSuffix"                           = var.dnsSuffix
+    "tags"                                = local.tags
+  }
+}
