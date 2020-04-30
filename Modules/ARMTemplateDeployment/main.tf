d209a58d4bf506728bf87f0ffb7469d7605ea0e8
Bhaskar Lokanathan <bhlokana@microsoft.com>
2020-04-30 14:32:59 -0500

    add parameters variable

--- Modules/ARMTemplateDeployment/main.tf
+++ Modules/ARMTemplateDeployment/main.tf
@@ -7,7 +7,6 @@ resource "azurerm_template_deployment" "template_deployment" {
   resource_group_name = var.resource_group_name
   deployment_mode     = var.deployment_mode == null ? "Incremental" : var.deployment_mode
   template_body       = file(var.template_file)
-  parameters_body     = file(var.parameters_file)
+  parameters_body     = file(var.parameters) ==null ? {} : file(var.parameters)
   parameters          = var.parameters == null ? {} : var.parameters
-
 }
--- Modules/ExternalAppServiceEnvironmentARM/main.tf
+++ Modules/ExternalAppServiceEnvironmentARM/main.tf
@@ -16,7 +16,7 @@ module "external_ase_arm_template" {
   resource_group_name = var.resource_group_name
   deployment_mode     = var.deployment_mode == null ? "Incremental" : var.deployment_mode
   template_body       = file(var.template_file)
-  parameters          = file(var.parameters) ==null ? {} : file(var.parameters)
+  parameters          = var.parameters
   parameters = {
     "aseName"                             = lower(module.naming.name)
     "aseLocation"                         = local.location
