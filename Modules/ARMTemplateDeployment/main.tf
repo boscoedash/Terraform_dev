7e2fa41962709db28e99c320696c843c933a88bf
Bhaskar Lokanathan <bhlokana@microsoft.com>
2020-04-30 13:48:01 -0500

    external app service environment

--- Modules/ARMTemplateDeployment/main.tf
+++ Modules/ARMTemplateDeployment/main.tf
@@ -3,5 +3,5 @@ resource "azurerm_template_deployment" "template_deployment" {
   resource_group_name = var.resource_group_name
   deployment_mode     = var.deployment_mode == null ? "Incremental" : var.deployment_mode
   template_body       = file(var.template_file)
-  parameters_body     = file(var.parameters_file)
+  parameters_body     = var.var.parameters_file == null ? {} : file(var.parameters_file)
 }
--- Modules/AppServiceEnvironmentARM/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "storageAccountName" {
-  value = azurerm_template_deployment.example.outputs["storageAccountName"]
-}
