db216efa5b65bece0a440fa37f4d2020e282a289
Bhaskar Lokanathan <bhlokana@microsoft.com>
2020-05-04 10:39:39 -0500

    output name

--- Modules/EventHub/outputs.tf
+++ Modules/EventHub/outputs.tf
@@ -1,7 +1,7 @@
 output "id" {
-  value = azurerm_event_hub.event_hub.id
+  value = azurerm_eventhub.event_hub.id
 }
 
 output "partition_ids" {
-  value = azurerm_event_hub.event_hub.partition_ids
+  value = azurerm_eventhub.event_hub.partition_ids
 }
