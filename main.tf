61fc52d3c5d1f592699877a2c3e3bff33d1c013e
Bhaskar Lokanathan <bhlokana@microsoft.com>
2020-05-06 13:14:14 -0500

    comment

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "azurerm_template_deployment" "template_deployment" {
   name                = "sqlmitestbl1"
   resource_group_name = var.resource_group_name
   deployment_mode     = var.deployment_mode == null ? "Incremental" : var.deployment_mode
-  parameters          = var.parameters
+  #parameters          = var.parameters
   template_body       = file("./Modules/SQLManagedInstance/SQLManagedInstance.json")
   parameters = {
     "name"                       = "sql-mi-test"
