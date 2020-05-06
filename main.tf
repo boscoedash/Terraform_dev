013e185aeeedecdc507e313c626214e03a2ea2ef
Bhaskar Lokanathan <bhlokana@microsoft.com>
2020-05-06 12:24:52 -0500

    rev

--- main.tf
+++ main.tf
@@ -58,30 +58,11 @@ module "sql_managed_instance_arm_template" {
   resource_group_name = var.resource_group_name
   deployment_mode     = "incremental"
   template_file       = var.template_file
-  parameters = {
-    "name"                       = lower(module.naming.name)
-    "location"                   = local.location
-    "tags"                       = local.tags
-    "skuName"                    = var.skuName
-    "skuSize"                    = var.skuSize == null ? "" : var.skuSize
-    "skuTier"                    = var.skuTier == null ? "Basic" : var.skuTier
-    "managedInstanceCreateMode"  = var.managedInstanceCreateMode == null ? "default" : var.managedInstanceCreateMode
-    "administratorLogin"         = var.administratorLogin
-    "administratorLoginPassword" = var.administratorLoginPassword
-    "subnetId"                   = var.subnetId
-    "licenseType"                = var.licenseType == null ? "LicenseIncluded" : var.licenseType
-    "vCores"                     = var.vCores == null ? 16 : var.vCores
-    "storageSizeInGB"            = var.storageSizeInGB ? 32 : var.storageSizeInGB
-    "collation"                  = var.collation == null ? "SQL_Latin1_General_CP1_CI_AS" : var.collation
-    "dnsZonePartner"             = var.dnsZonePartner == null ? "" : var.dnsZonePartner
-    "publicDataEndpointEnabled"  = var.publicDataEndpointEnabled == null ? false : var.publicDataEndpointEnabled
-    "sourceManagedInstanceId"    = var.sourceManagedInstanceId == null ? "" : var.sourceManagedInstanceId
-    "restorePointInTime"         = var.restorePointInTime == null ? "" : var.restorePointInTime
-    "proxyOverride"              = var.proxyOverride == null ? "" : var.proxyOverride
-    "timezoneId"                 = var.timezoneId == null ? "UTC" : var.timezoneId
-    "instancePoolId"             = var.instancePoolId == null ? "" : var.instancePoolId
-    "minimalTlsVersion"          = var.minimalTlsVersion == null ? "" : var.instancePoolId
-  }
+  parameters          = var.parameters
+  depends_on = [
+    module.ResourceGroup
+    module.Subnet
+  ]
 }
 
 /*
