61191cab31d84603afe3d1047543a6661d05c28d
bhlokana <bhlokana@microsoft.com>
2021-02-19 11:44:24 -0600

    update ase module

--- Modules/AppServiceEnvironment/main.tf
+++ Modules/AppServiceEnvironment/main.tf
@@ -1,14 +1,9 @@
-module "naming" {
-  source      = "../../../naming/standard-name"
-  counter     = var.counter
-  description = var.description
-  location    = local.location
-  type        = "ase"
-}
-
 resource "azurerm_app_service_environment" "app_service_environment" {
-  name                   = lower(module.naming.name)
-  subnet_id              = var.subnet_id
-  pricing_tier           = var.pricing_tier
-  front_end_scale_factor = var.front_end_scale_factor
+  name                         = var.app_service_environment_name
+  resource_group_name          = var.resource_group_name
+  subnet_id                    = var.subnet_id
+  pricing_tier                 = var.ase_pricing_tier
+  front_end_scale_factor       = var.front_end_scale_factor
+  internal_load_balancing_mode = var.internal_load_balancing_mode
+  allowed_user_ip_cidrs        = var.allowed_user_ip_cidrs
 }
--- Modules/AppServiceEnvironment/variables.tf
+++ Modules/AppServiceEnvironment/variables.tf
@@ -1,16 +1,6 @@
-variable "location" {
+variable "app_service_environment_name" {
   type        = string
-  description = "The ASE location."
-}
-
-variable "description" {
-  type        = string
-  description = "The description of the resource for naming."
-}
-
-variable "counter" {
-  type    = number
-  default = 1
+  description = "The ASE name."
 }
 
 variable "resource_group_name" {
@@ -23,12 +13,26 @@ variable "subnet_id" {
   description = "The kind of App Service plan to create.  i.e. Windows, Linux, FunctionAll."
 }
 
-variable "pricing_tier" {
+variable "ase_pricing_tier" {
   type        = string
   description = "The pricing tier to use for the plan."
+  default     = "I1"
 }
 
 variable "front_end_scale_factor" {
   type        = number
   description = "The ASE scale factor."
+  default     = 15
+}
+
+variable "internal_load_balancing_mode" {
+  type        = string
+  description = "Specifies which endpoints to serve internally in the Virtual Network for the App Service Environment. Possible values are None, Web, Publishing and combined value Web, Publishing. Defaults to None."
+  default     = "None"
+}
+
+variable "allowed_user_ip_cidrs" {
+  type        = list
+  description = "Allowed user added IP ranges on the ASE database. Use the addresses you want to set as the explicit egress address ranges"
+  default     = []
 }
