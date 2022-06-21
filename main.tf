b92641f2c224c956804942c59dca5bfd17f18d93
bhlokana@microsoft.com <bhlokana@microsoft.com>
2022-06-21 17:34:04 -0500

    add tfstate

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+    backend "azurerm" {
+        resource_group_name  = "tfstate"
+        storage_account_name = "bhlokanaversion"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
+
+}
+
 provider "azurerm" {
   features {}
 }
